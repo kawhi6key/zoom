1a4aed13015341253cad32d6cfc3ee163f1cf4f7
kawhi6key <hi6k0.2yk0@gmail.com>
2022-01-08 00:44:48 +0900

    Add new Program

--- zoom.py
+++ zoom.py
@@ -10,40 +10,60 @@ def main():
     conn = sqlite3.connect('zoom_url.db')
     c = conn.cursor()
 
-    # sql文（今は1つの行だけ取り出す）
+    # テーブル間の比較を行う
     c.execute('''
-        SELECT *
-        FROM zoom_url
-        LIMIT 1''')
-    # リストをつくる
-    reservation_list = c.fetchone()
-    # データベース閉じる
-    conn.close()
+            SELECT *
+            FROM zoom_url
+            EXCEPT
+            SELECT *
+            FROM reservation_zoomData;''')
+    difference_list = c.fetchone()
+    # print(difference_list)
 
-    time_now = datetime.datetime.now()
-    expireation_time = time_now + datetime.timedelta(seconds=20)
-    rounded_off_exp_time = round(expireation_time.timestamp())
+    try:
+        t = (difference_list[0],)
+        c.execute('SELECT * FROM zoom_url WHERE event_id=?;', t)
+        reservation_list = c.fetchone()
+        # データベース閉じる
+        conn.close()
+    except TypeError:
+        reservation_list = []
 
-    headers = {"alg": "HS256" , "typ": "JWT"}
-    payload = {"iss": settings.apiKey() , "exp": rounded_off_exp_time}
-    encoded_jwt = jwt.encode(payload, settings.apiSecret(), algorithm="HS256")
-    email = settings.email()
+    if reservation_list is not None:
+        try:
+            time_now = datetime.datetime.now()
+            expireation_time = time_now + datetime.timedelta(seconds=20)
+            rounded_off_exp_time = round(expireation_time.timestamp())
 
-    url = f"https://api.zoom.us/v2/users/{email}/meetings"
+            headers = {"alg": "HS256" , "typ": "JWT"}
+            payload = {"iss": settings.apiKey() , "exp": rounded_off_exp_time}
+            encoded_jwt = jwt.encode(payload, settings.apiSecret(), algorithm="HS256")
+            email = settings.email()
 
-    date = datetime.datetime.fromisoformat(reservation_list[3][:-6]).strftime("%Y-%m-%dT%H: %M: %S")
-    event_time_H = datetime.time.fromisoformat(reservation_list[5]).strftime("%H")
-    event_time_H = int(event_time_H)*60
-    event_time_M = datetime.time.fromisoformat(reservation_list[5]).strftime("%M")
-    event_time_M = int(event_time_M)
-    event_time = event_time_H + event_time_M
+            url = f"https://api.zoom.us/v2/users/{email}/meetings"
 
-    obj = {"topic": reservation_list[1], "start_time": date, "duration": event_time, "password": "12345"}
-    header = {"authorization": f"Bearer{encoded_jwt}"}
-    creat_meeting = requests.post(url, json=obj, headers=header)
-    print(creat_meeting.text)
-    # # データベースに格納する
-    # conn = sqlite3.connect('zoom_url.db')
-    # c = conn.cursor()
-    # c.execute("insert into zoom_meetings values(?, ?, ?, ?, ?, ?)",(creat_meeting['id'], creat_meeting['summary'], creat_meeting['description'], creat_meeting['start'], creat_meeting['end'], creat_meeting['druation']))
-    # conn.commit()
+            date = datetime.datetime.fromisoformat(reservation_list[3]).strftime("%Y-%m-%dT%H: %M: %S")
+            event_time_H = datetime.time.fromisoformat(reservation_list[5]).strftime("%H")
+            event_time_H = int(event_time_H)*60
+            event_time_M = datetime.time.fromisoformat(reservation_list[5]).strftime("%M")
+            event_time_M = int(event_time_M)
+            event_time = event_time_H + event_time_M
+
+            obj = {"topic": reservation_list[1], "start_time": date, "duration": event_time, "password": "12345"}
+            header = {"authorization": f"Bearer{encoded_jwt}"}
+            creat_meeting = requests.post(url, json=obj, headers=header)
+            print(creat_meeting.text)
+            # データベースに接続
+            conn = sqlite3.connect('zoom_url.db')
+            c = conn.cursor()
+            t = (reservation_list[0], reservation_list[1], reservation_list[2], reservation_list[3], reservation_list[4], reservation_list[5])
+            c.execute("INSERT INTO reservation_zoomData VALUES(?, ?, ?, ?, ?, ?)", t)
+            # 保存してデータベースを閉じる
+            conn.commit()
+            conn.close()
+        except IndexError:
+            pass
+    else:
+        pass
+
+main()
